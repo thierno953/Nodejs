56b2ac59f138d39eea33f3aea281717f2a1965ed
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-05-02 16:31:16 +0200

    jobs

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-// packages imports
+//packages imports
 import express from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
 import colors from "colors";
 import cors from "cors";
 import morgan from "morgan";
+
+//securty packages
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 
 //files imports
 import connectDB from "./config/db.js";
@@ -13,29 +18,33 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
+import jobsRoutes from "./routes/jobsRoute.js";
 
 //Dot ENV config
 dotenv.config();
 
-//mongodb connection  
+//mongodb connection
 connectDB();
 
 //rest object
 const app = express();
 
 //middelwares
+app.use(helmet(``));
+app.use(xss());
+app.use(mongoSanitize());
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
 //routes
-// app.get("/", (req, res) => {
-//   res.send("Server is running...");
-// });
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
 
-//routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/job", jobsRoutes);
 
 //validation middleware
 app.use(errorMiddleware);
